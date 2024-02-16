e2f8ba6184c5e35f03799b0ab45cbd0e3aa61fcb
rinaa040 <156610253+rinaa040@users.noreply.github.com>
2024-02-16 17:19:33 +0400

    Add files via upload

---
+++ args.py
@@ -0,0 +1,13 @@
+import argparse
+import requests
+
+parser = argparse.ArgumentParser()
+parser.add_argument('server', type=str)
+parser.add_argument('port', type=str, help="port")
+parser.add_argument('key', type=str, help="key of dict")
+parser.add_argument('--digits', type=int, default=1)
+parser.add_argument('--mult', type=int, default=1)
+
+args = parser.parse_args()
+response = requests.get(f"http://{args.server}/{args.port}").json()
+print(args.key)
